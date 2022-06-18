d27f10f1d0ed5a39b199bd65afe2e56fa80abf94
swipss <vassiljevsten1@gmail.com>
2022-06-18 08:26:36 +0300

    create center banner

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -13,7 +13,10 @@ const Sidebar = () => {
   const { data: session, status } = useSession();
   console.log(session);
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <div
+      className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen
+    "
+    >
       <div className="space-y-4">
         <button
           onClick={() => signOut()}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
+import Center from "../components/Center";
 import Sidebar from "../components/Sidebar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
-      <main className="">
+      <main className="flex">
         <Sidebar />
+        <Center />
       </main>
 
       {/* <div>player</div> */}
