364e49892d8db4b71fbfff3449a613aa93472529
swipss <vassiljevsten1@gmail.com>
2022-06-18 04:21:43 +0300

    sign in page

--- pages/login.js
+++ pages/login.js
@@ -3,8 +3,22 @@ import { getProviders, signIn } from "next-auth/react";
 
 const Login = ({ providers }) => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center space-y-5">
+      <img
+        src="https://links.papareact.com/9xl"
+        className="w-52= h-52"
+        alt=""
+      />
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-[#18d860] text-white p-5 rounded-full"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
