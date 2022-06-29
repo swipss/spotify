c63d883f14326fbcf45ea69011b36a5564f7c06f
swipss <vassiljevsten1@gmail.com>
2022-06-29 12:22:22 +0300

    change middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,22 +2,15 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req) {
-  const url = req.nextUrl.clone();
-  url.pathname = "/login";
-  // token will exist if the user is logged in
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
-  const { pathname } = req.nextUrl;
-
-  // ALlow the request if the following is true:
-  // 1) it's a request to next-auth session
-  // 2) the token exists
-
+  const { pathname, origin } = req.nextUrl;
+  // allow request if
+  // 1) token exists
   if (pathname.includes("/api/auth") || token) {
     return NextResponse.next();
   }
 
-  // redirect to login page if the user is not logged in and they are requesting a protected routeing a protected route
-  if (!token && pathname !== url.pathname) {
-    return NextResponse.rewrite(url);
+  if (!token && pathname !== "/login") {
+    return NextResponse.redirect(`${origin}/login`);
   }
 }
